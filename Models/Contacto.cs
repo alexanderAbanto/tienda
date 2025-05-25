43393c696dce1747b128388322aa46fd4c7b1320
alexanderAbanto <alexanderabanto19@hotmail.com>
2025-05-25 18:25:00 -0500

    Initial Report

---
+++ Models/Contacto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apptienda.Models
+{
+    public class Contacto
+    {
+        public string? Nombres { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Mensaje { get; set; }
+    }
+}
