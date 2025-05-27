496742d579c0260408d23c544dfef054edc35468
alexanderAbanto <alexanderabanto19@hotmail.com>
2025-05-27 17:21:13 -0500

    contacto

--- Models/Contacto.cs
+++ Models/Contacto.cs
@@ -2,15 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
 
 namespace apptienda.Models
 {
     public class Contacto
     {
+        [Required(ErrorMessage = "Nombres son Obligatorios")]
+        [NotNull]
         public string? Nombres { get; set; }
-
+        [NotNull]
         public string? Email { get; set; }
-
+        [NotNull]
         public string? Mensaje { get; set; }
     }
 }
